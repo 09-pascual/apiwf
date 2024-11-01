0e3e2e36820b6298e17cbb1c5b24d25b69edf4bb
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-11-01 04:32:07 -0400

    fixed register.py, testing endpoint

--- workflowapiapi/views/register.py
+++ workflowapiapi/views/register.py
@@ -1,63 +1,30 @@
 import json
-from django.http import HttpResponse, HttpResponseNotAllowed
-from django.contrib.auth import authenticate
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import status
+from django.contrib.auth.models import User as AuthUser
+from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from workflowapiapi.models import User
 
-@csrf_exempt
-def login_user(request):
-    '''Handles the authentication of a user
-
-    Method arguments:
-      request -- The full HTTP request object
-    '''
-
-    if request.method == 'POST':
-        body = request.body.decode('utf-8')
-        req_body = json.loads(body)
-
-        # Use the built-in authenticate method to verify
-        username = req_body['username']
-        password = req_body['password']
-        authenticated_user = authenticate(username=username, password=password)
-
-        # If authentication was successful, respond with their token
-        if authenticated_user is not None:
-            token = Token.objects.get(user=authenticated_user)
-            data = json.dumps({
-                "valid": True, 
-                "token": token.key, 
-                "id": authenticated_user.id,
-                "username": authenticated_user.username,
-                "role": authenticated_user.role
-            })
-            return HttpResponse(data, content_type='application/json')
-
-        else:
-            # Bad login details were provided. So we can't log the user in.
-            data = json.dumps({"valid": False})
-            return HttpResponse(data, content_type='application/json')
-
-    return HttpResponseNotAllowed(permitted_methods=['POST'])
+# With this file I can use to create auth tokens, auth_user with password without it having to be in model 
 
 @csrf_exempt
 def register_user(request):
-    '''Handles the creation of a new user for authentication
-
-    Method arguments:
-      request -- The full HTTP request object
-    '''
-
-    # Load the JSON string of the request body into a dict
-    req_body = json.loads(request.body.decode())
-
-    # Create a new user by invoking the `create_user` helper method
     try:
-        new_user = User.objects.create(
+        req_body = json.loads(request.body.decode())
+        
+        # First create auth user for password/token
+        auth_user = AuthUser.objects.create_user(
             username=req_body['username'],
-            password=req_body['password'],  # Note: This should be hashed
+            password=req_body['password']
+        )
+        
+        # Create token
+        token = Token.objects.create(user=auth_user)
+        
+        # Create your custom user without password
+        custom_user = User.objects.create(
+            username=req_body['username'],
             first_name=req_body['first_name'],
             last_name=req_body['last_name'],
             birth_date=req_body['birth_date'],
@@ -66,25 +33,59 @@ def register_user(request):
             role=req_body['role']
         )
         
-        # Hash the password
-        new_user.set_password(req_body['password'])
-        new_user.save()
-
-        # Use the REST Framework's token generator on the new user account
-        token = Token.objects.create(user=new_user)
-
-        # Return the token to the client
         data = json.dumps({
-            "token": token.key, 
-            "id": new_user.id,
             "valid": True,
-            "role": new_user.role
+            "token": token.key,
+            "user_id": custom_user.id,
+            "username": custom_user.username,
+            "role": custom_user.role
         })
-        return HttpResponse(data, content_type='application/json', status=status.HTTP_201_CREATED)
-    
+        return HttpResponse(data, content_type='application/json')
+            
+    except Exception as ex:
+        # If anything fails, cleanup any created users
+        if 'auth_user' in locals():
+            auth_user.delete()
+        if 'custom_user' in locals():
+            custom_user.delete()
+            
+        data = json.dumps({
+            "valid": False,
+            "error": str(ex)
+        })
+        return HttpResponse(data, content_type='application/json')
+
+@csrf_exempt
+def login_user(request):
+    try:
+        body = request.body.decode('utf-8')
+        req_body = json.loads(body)
+        
+        # Authenticate using auth_user
+        auth_user = authenticate(username=req_body['username'], 
+                               password=req_body['password'])
+        
+        if auth_user is not None:
+            # Get your custom user
+            custom_user = User.objects.get(username=auth_user.username)
+            # Get or create token
+            token, created = Token.objects.get_or_create(user=auth_user)
+            
+            data = json.dumps({
+                "valid": True,
+                "token": token.key,
+                "user_id": custom_user.id,
+                "username": custom_user.username,
+                "role": custom_user.role
+            })
+            return HttpResponse(data, content_type='application/json')
+        else:
+            data = json.dumps({"valid": False})
+            return HttpResponse(data, content_type='application/json')
+            
     except Exception as ex:
         data = json.dumps({
             "valid": False,
             "error": str(ex)
         })
-        return HttpResponse(data, content_type='application/json', status=status.HTTP_400_BAD_REQUEST)
+        return HttpResponse(data, content_type='application/json')
