b1e6288c33d2ee0f4e80b868a45f05c59c714904
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-11-06 13:18:19 -0500

    updated serializers to show all project details

--- workflowapiapi/views/project_group.py
+++ workflowapiapi/views/project_group.py
@@ -1,11 +1,23 @@
 from rest_framework import viewsets, serializers
 from workflowapiapi.models import ProjectGroup
+from .group import GroupSerializer
+
+class ProjectGroupReadSerializer(serializers.ModelSerializer):
+    group = GroupSerializer()
+    
+    class Meta:
+        model = ProjectGroup
+        fields = ['id', 'group', 'project']
 
 class ProjectGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectGroup
         fields = ['id', 'group', 'project']
-
+        
 class ProjectGroupViewSet(viewsets.ModelViewSet):
     queryset = ProjectGroup.objects.all()
-    serializer_class = ProjectGroupSerializer
+    
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:  # When reading data
+            return ProjectGroupReadSerializer
+        return ProjectGroupSerializer
