fffa7173365602d0eed5c485662f4729b421d333
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-11-07 11:17:08 -0500

    changed to show group and client informaiton

--- workflowapiapi/views/group.py
+++ workflowapiapi/views/group.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets, serializers
 from workflowapiapi.models import Group
+from .user import UserSerializer
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ['id', 'name', 'description', 'groupworker_set', 'projectgroup_set']
-        depth = 2  # This will automatically serialize nested relationships up to 2 levels deep
+        depth = 3  # This will automatically serialize nested relationships up to 2 levels deep
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
