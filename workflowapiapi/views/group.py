c6c81411ebbef4328e139155d763ed4f0e6aff11
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-11-07 02:31:22 -0500

    trying to change serializers to show show information needed

--- workflowapiapi/views/group.py
+++ workflowapiapi/views/group.py
@@ -4,8 +4,8 @@ from workflowapiapi.models import Group
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ['id', 'name', 'description']
-        # Note: 'workers' and 'projects' will be handled through junction tables
+        fields = ['id', 'name', 'description', 'groupworker_set', 'projectgroup_set']
+        depth = 2  # This will automatically serialize nested relationships up to 2 levels deep
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
--- workflowapiapi/views/group_worker.py
+++ workflowapiapi/views/group_worker.py
@@ -1,5 +1,13 @@
 from rest_framework import viewsets, serializers
 from workflowapiapi.models import GroupWorker
+from .group import GroupSerializer
+
+class GroupWorkerReadSerializer(serializers.ModelSerializer):
+    group = GroupSerializer()
+
+    class Meta:
+        model = GroupWorker
+        fields = ['id', 'group', 'worker']
 
 class GroupWorkerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,4 +16,8 @@ class GroupWorkerSerializer(serializers.ModelSerializer):
 
 class GroupWorkerViewSet(viewsets.ModelViewSet):
     queryset = GroupWorker.objects.all()
-    serializer_class = GroupWorkerSerializer
+    
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return GroupWorkerReadSerializer
+        return GroupWorkerSerializer
