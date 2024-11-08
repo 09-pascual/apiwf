2f63682c454983a0050e76b59ae2ed220597d84b
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-11-08 00:05:55 -0500

    can now see edit form for group

--- workflowapiapi/views/worker.py
+++ workflowapiapi/views/worker.py
@@ -2,9 +2,6 @@ from rest_framework import viewsets, serializers
 from workflowapiapi.models import Worker, User
 from .user import UserSerializer
 
-
-
-  
 class WorkerReadSerializer(serializers.ModelSerializer):
     user = UserSerializer()
 
@@ -34,10 +31,36 @@ class WorkerWriteSerializer(serializers.ModelSerializer):
         
         return instance
 
+class WorkerCreateSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Worker
+        fields = ['id', 'user', 'availability_status']
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create_user(  # Use create_user instead of create
+            username=user_data['username'],
+            password=user_data['password'],
+            first_name=user_data['first_name'],
+            last_name=user_data['last_name'],
+            birth_date=user_data['birth_date'],
+            phone_number=user_data['phone_number'],
+            nickname=user_data['nickname'],
+            role='worker'  # Set role explicitly
+        )
+        
+        # Create and return Worker instance
+        worker = Worker.objects.create(user=user, **validated_data)
+        return worker
+
 class WorkerViewSet(viewsets.ModelViewSet):
     queryset = Worker.objects.all()
     
     def get_serializer_class(self):
-        if self.action in ['create', 'update', 'partial_update']:
+        if self.action == 'create':
+            return WorkerCreateSerializer
+        if self.action in ['update', 'partial_update']:
             return WorkerWriteSerializer
         return WorkerReadSerializer
